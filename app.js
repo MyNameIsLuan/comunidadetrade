61580337e3dba4929f3aa8559647856b1179faae
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-09-07 18:10:43 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('./config.json')
+
+client.on('message', message => {
+  if (message.author.bot) return
+  if (!message.content.startsWith(config.prefix)) return
+  let command = message.content.split(' ')[0]
+  command = command.slice(config.prefix.length)
+  let args = message.content.split(' ').slice(1)
+  try {
+    let commandFile = require(`./comandos/${command}.js`)
+    commandFile.run(client, message, args)
+  } catch (err) {
+    console.error(err)
+  }
+
+  client.on('ready', () => {
+    console.log(`Bot iniciado com sucesso!`)
+  })
+})
+client.login(process.env.TOKEN)
