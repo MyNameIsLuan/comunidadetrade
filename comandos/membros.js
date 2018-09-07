fdd593002f59300dc447bd52ee767f450676f48f
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-09-07 18:31:51 -0300

    Update membros.js

--- comandos/membros.js
+++ comandos/membros.js
@@ -2,3 +2,4 @@ var Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
 message.channel.send(`Temos atualmente ${member.guild.memberCount} membros no grupo!`)
+}
